abd25f3e56d6a4e9d5ac6744a90d4e77a22b72e6
arwabadri <arwa.badri.aiesec@gmail.com>
2024-06-09 13:04:39 +0100

    TRYINGgit status

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          It's my project
+          I wanna make update here!!
         </p>
       </header>
     </div>
